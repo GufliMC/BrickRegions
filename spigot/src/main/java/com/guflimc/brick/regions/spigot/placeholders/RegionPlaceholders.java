19d6283d0ecf9968fb675553f8f0054838101ae8
Joris Guffens <joris.guffens@outlook.com>
2023-04-09 22:57:57 +0200

    updated placeholders

--- spigot/src/main/java/com/guflimc/brick/regions/spigot/placeholders/RegionPlaceholders.java
+++ spigot/src/main/java/com/guflimc/brick/regions/spigot/placeholders/RegionPlaceholders.java
@@ -1,26 +1,30 @@
 package com.guflimc.brick.regions.spigot.placeholders;
 
+import com.guflimc.brick.placeholders.api.module.BasePlaceholderModule;
+import com.guflimc.brick.placeholders.api.resolver.PlaceholderResolver;
 import com.guflimc.brick.placeholders.spigot.api.SpigotPlaceholderAPI;
 import com.guflimc.brick.regions.api.domain.Region;
 import com.guflimc.brick.regions.spigot.api.SpigotRegionAPI;
 import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
 
 import java.util.Comparator;
+import java.util.function.Function;
 
 public class RegionPlaceholders {
 
-    public static void init() {
-        SpigotPlaceholderAPI.get().registerReplacer("region_name", (player) ->
-                SpigotRegionAPI.get().regionsAt(player).stream()
+    private static <T> PlaceholderResolver<Player, T> region(Function<Region, T> map) {
+        return PlaceholderResolver.requireEntity((placeholder, context) ->
+                SpigotRegionAPI.get().regionsAt(context.entity()).stream()
                         .max(Comparator.comparing(Region::priority))
-                        .map(rg -> Component.text(rg.name())).orElse(null)
-        );
+                        .map(map).orElse(null));
+    }
 
-        SpigotPlaceholderAPI.get().registerReplacer("region_display_name", (player) ->
-                SpigotRegionAPI.get().regionsAt(player).stream()
-                        .max(Comparator.comparing(Region::priority))
-                        .map(Region::displayName).orElse(null)
-        );
+    public static void init() {
+        BasePlaceholderModule<Player> module = new BasePlaceholderModule<>("region");
+        module.register("name", region(region -> Component.text(region.name())));
+        module.register("display_name", region(Region::displayName));
+        SpigotPlaceholderAPI.get().register(module);
     }
 
 }
